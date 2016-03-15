79b9590ec6fb88855f4a22622a1fe0c27df704ea
thevietto <thevietto@gmail.com>
2016-03-16 00:11:41 +0300

    [JBDS-1] just another init commit

---
+++ jbds-service/src/test/java/example/SampleTest.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void sampleTest() {
+        Assert.assertTrue(2 + 2 == 4);
+    }
+
+}
