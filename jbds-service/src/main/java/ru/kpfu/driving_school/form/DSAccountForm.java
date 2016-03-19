4012ee34bae875f9010a65c842c9564c9d7fcfaa
etovladislav <etovladislav@yandex.ru>
2016-03-19 18:06:58 +0300

    [JBDS-8] creating a driving school accounts

---
+++ jbds-service/src/main/java/ru/kpfu/driving_school/form/DSAccountForm.java
@@ -0,0 +1,31 @@
+package ru.kpfu.driving_school.form;
+
+/**
+ * Created by etovladislav on 19.03.16.
+ */
+public class DSAccountForm {
+
+    private String login;
+
+    private String password;
+
+    public DSAccountForm() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
