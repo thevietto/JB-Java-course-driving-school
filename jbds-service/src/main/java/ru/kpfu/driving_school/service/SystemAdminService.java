7aa35ac3a5b33ac6f4906a0146d2aaa17a51f20d
etovladislav <etovladislav@yandex.ru>
2016-03-21 18:03:51 +0300

    [JBDS-10] - created system admin service

---
+++ jbds-service/src/main/java/ru/kpfu/driving_school/service/SystemAdminService.java
@@ -0,0 +1,7 @@
+package ru.kpfu.driving_school.service;
+
+/**
+ * Created by etovladislav on 21.03.16.
+ */
+public interface SystemAdminService {
+}
---
+++ jbds-service/src/main/java/ru/kpfu/driving_school/service/impl/SystemAdminServiceImpl.java
@@ -0,0 +1,9 @@
+package ru.kpfu.driving_school.service.impl;
+
+import ru.kpfu.driving_school.service.SystemAdminService;
+
+/**
+ * Created by etovladislav on 21.03.16.
+ */
+public class SystemAdminServiceImpl implements SystemAdminService {
+}
