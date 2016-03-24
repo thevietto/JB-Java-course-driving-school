275ae7f247fcac165cae341ffd3bf7f8142a0553
Brutaller <kazkachi1@gmail.com>
2016-03-24 18:33:49 +0300

    [JBDS-4] .xlsx files can be uploaded

--- jbds-service/src/main/java/ru/kpfu/driving_school/util/ExcelStudentParser.java
+++ jbds-service/src/main/java/ru/kpfu/driving_school/util/ExcelStudentParser.java
@@ -3,6 +3,9 @@ package ru.kpfu.driving_school.util;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import ru.kpfu.driving_school.form.StudentForm;
@@ -18,9 +21,9 @@ import java.util.List;
 public class ExcelStudentParser {
 
     public List<StudentForm> parse(MultipartFile file) throws IOException {
-        HSSFWorkbook workbook = null;
         List<StudentForm> students = new ArrayList<>();
-            workbook = new HSSFWorkbook(file.getInputStream());
+        if (file.getOriginalFilename().endsWith(".xls")) {
+            HSSFWorkbook workbook = new HSSFWorkbook(file.getInputStream());
             HSSFSheet sheet = workbook.getSheetAt(0);
             for (int i = 0; i <= sheet.getLastRowNum(); i++) {
                 HSSFRow row = sheet.getRow(i);
@@ -32,6 +35,20 @@ public class ExcelStudentParser {
                 studentForm.setSurname(row.getCell(0).getStringCellValue());
                 students.add(studentForm);
             }
+        } else if (file.getOriginalFilename().endsWith(".xlsx")){
+            XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            for (int i = 0; i <= sheet.getLastRowNum(); i++) {
+                XSSFRow row = sheet.getRow(i);
+
+                StudentForm studentForm = new StudentForm();
+
+                studentForm.setLastname(row.getCell(2).getStringCellValue());
+                studentForm.setFirstname(row.getCell(1).getStringCellValue());
+                studentForm.setSurname(row.getCell(0).getStringCellValue());
+                students.add(studentForm);
+            }
+        }
 
         return students;
     }
