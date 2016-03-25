fcebfef912973a0088882f652a01a0453aeb6707
Alexander Pliskin <alexnetmore@gmail.com>
2016-03-25 18:45:39 +0300

    [JBDS-6] some changes

--- jbds-domain/src/main/java/ru/kpfu/driving_school/repository/StudentGroupRepository.java
+++ jbds-domain/src/main/java/ru/kpfu/driving_school/repository/StudentGroupRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import ru.kpfu.driving_school.model.Credentials;
 import ru.kpfu.driving_school.model.StudentGroup;
 
 import java.util.List;
