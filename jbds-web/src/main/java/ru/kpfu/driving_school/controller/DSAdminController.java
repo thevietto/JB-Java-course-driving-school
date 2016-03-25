2d9094ea9d2b7fe488c92a18fb078b279b42554a
Alexander Pliskin <alexnetmore@gmail.com>
2016-03-25 16:07:56 +0300

    [JBDS-6] some changes

--- jbds-web/src/main/java/ru/kpfu/driving_school/controller/DSAdminController.java
+++ jbds-web/src/main/java/ru/kpfu/driving_school/controller/DSAdminController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import ru.kpfu.driving_school.form.StudentForm;
 import ru.kpfu.driving_school.service.DSAdminService;
