c81e1ef698beae7f91853f0c7c159d0682398083
Brutaller <kazkachi1@gmail.com>
2016-03-25 16:29:47 +0300

    [JBDS-4] removed excess code

--- jbds-service/src/main/java/ru/kpfu/driving_school/util/ExcelStudentParser.java
+++ jbds-service/src/main/java/ru/kpfu/driving_school/util/ExcelStudentParser.java
@@ -1,10 +1,9 @@
 package ru.kpfu.driving_school.util;
 
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,34 +21,29 @@ public class ExcelStudentParser {
 
     public List<StudentForm> parse(MultipartFile file) throws IOException {
         List<StudentForm> students = new ArrayList<>();
+
+        Workbook workbook;
+
         if (file.getOriginalFilename().endsWith(".xls")) {
-            HSSFWorkbook workbook = new HSSFWorkbook(file.getInputStream());
-            HSSFSheet sheet = workbook.getSheetAt(0);
-            for (int i = 0; i <= sheet.getLastRowNum(); i++) {
-                HSSFRow row = sheet.getRow(i);
-
-                StudentForm studentForm = new StudentForm();
-
-                studentForm.setLastname(row.getCell(2).getStringCellValue());
-                studentForm.setFirstname(row.getCell(1).getStringCellValue());
-                studentForm.setSurname(row.getCell(0).getStringCellValue());
-                students.add(studentForm);
-            }
-        } else if (file.getOriginalFilename().endsWith(".xlsx")){
-            XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            for (int i = 0; i <= sheet.getLastRowNum(); i++) {
-                XSSFRow row = sheet.getRow(i);
-
-                StudentForm studentForm = new StudentForm();
-
-                studentForm.setLastname(row.getCell(2).getStringCellValue());
-                studentForm.setFirstname(row.getCell(1).getStringCellValue());
-                studentForm.setSurname(row.getCell(0).getStringCellValue());
-                students.add(studentForm);
-            }
+            workbook = new HSSFWorkbook(file.getInputStream());
+        } else if (file.getOriginalFilename().endsWith(".xlsx")) {
+            workbook = new XSSFWorkbook(file.getInputStream());
+        } else {
+            throw new IOException("Wrong file extension");
         }
 
-        return students;
+        Sheet sheet = workbook.getSheetAt(0);
+
+        for (int i = 0; i <= sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+
+            StudentForm studentForm = new StudentForm();
+
+            studentForm.setLastname(row.getCell(2).getStringCellValue());
+            studentForm.setFirstname(row.getCell(1).getStringCellValue());
+            studentForm.setSurname(row.getCell(0).getStringCellValue());
+            students.add(studentForm);
+        }
+            return students;
     }
 }
